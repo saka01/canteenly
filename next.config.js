bc1c386c9282d576fe8e9d012cab003efd729a8e
Detonic1 <princedan4005@gmail.com>
2024-01-29 23:57:58 -0500

    first

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-//   env: {
-//     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-//     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-//     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-//     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-//     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-//   },
-};
-
-module.exports = nextConfig
+    //   env: {
+    //     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    //     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    //     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    //     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    //     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    //   },
+    };
+    
+    module.exports = nextConfig
