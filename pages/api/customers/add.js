2cc6a91778c300a12c6ed79c16ebfe6b3878f922
Detonic1 <princedan4005@gmail.com>
2024-02-06 00:49:29 -0500

    removedprismo/client

--- pages/api/customers/add.js
+++ pages/api/customers/add.js
@@ -1,24 +1,15 @@
 import { auth } from "../../../lib/actions";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { setCookie } from "nookies";
-const { PrismaClient } = require("@prisma/client");
 
 export default async function handler(req, res) {
-  const prisma = new PrismaClient();
   if (req.method !== "POST") {
     return res.status(405).end("Method Not Allowed");
   }
 
   try {
     console.log("almost there");
-    const newUser = await prisma.user.create({
-      data: {
-        firebaseId: "some-firebase-id",
-        email: "user@example.com",
-        name: "User Name",
-      },
-    });
-    console.log(newUser);
+      
     res.status(200).json({ message: "Added successfully" });
   } catch (error) {
     res.status(401).json({ error: error.message });
